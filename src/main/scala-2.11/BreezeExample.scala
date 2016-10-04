2941cc0ec8bda11986fe1526bb79a23b71196ac0
Alessandro Valcepina <valcepina@gmail.com>
2016-10-04 16:33:01 +0200

    Experimenting with matrix slicing in order to calculate inner average

--- src/main/scala-2.11/BreezeExample.scala
+++ src/main/scala-2.11/BreezeExample.scala
@@ -1,6 +1,7 @@
-import breeze.linalg.{CSCMatrix, DenseMatrix}
+import breeze.linalg.{CSCMatrix, DenseMatrix, SliceMatrix, View}
 import java.io._
 
+import breeze.signal._
 import breeze.linalg
 
 /**
@@ -13,29 +14,53 @@ object BreezeExample extends App {
   //denseMatrix(0 , 0 ) = 10;
 
   //Read matrix from csv file
-  val denseMatrix= getMatrix("/denseMatrix.csv")
+  val denseMatrix = getMatrix("/denseMatrix.csv")
 
   // println(denseMatrix)
 
-  val m1 = CSCMatrix.create(denseMatrix.rows, denseMatrix.cols,denseMatrix.toArray)
+  val m1 = CSCMatrix.create(denseMatrix.rows, denseMatrix.cols, denseMatrix.toArray)
 
-  val m2 = m1.map(filterValue(_, 5.0))
+  //val m2 = m1.map(filterValue(_, 5.0))
+
+  //println(m3)
 
   //Size of active elements
   println(m1.activeSize)
 
   //Set one value manually
-  m1(0,1) = 20
+  m1(0, 1) = 20
 
-  //Size of active elements for filtered table
-  println(m2.activeSize)
+  val m2 = m1.active.values.map(filterValue(_, 5.0))
 
   //Print filtered matrix
   println(m2)
 
-  def getMatrix(path : String) = linalg.csvread(new File(getClass.getResource(path).getPath),',')
+  //Size of active elements for filtered table
+  println(m2.activeSize)
 
-  def filterValue(x : Double, thresold : Double) = if (x < thresold) 0.0 else x
+  val m3 = matrixAverage(m2)
+
+  println(m3.toDense)
+
+  def getMatrix(path: String) = linalg.csvread(new File(getClass.getResource(path).getPath), ',')
+
+  def filterValue(x: Double, thresold: Double) = if (x < thresold) 0.0 else x
+
+  def matrixAverage(matrix: CSCMatrix[Double]) = {
+
+    var result = CSCMatrix.zeros[Double](matrix.rows, matrix.cols)
+
+    val ccc = matrix.active.pairs.map(v => {
+
+      result(v._1._1, v._1._2) = v._2 + sliceMatrix(matrix, v._1._1, v._1._2).flatten(View.Copy).sum
+
+    })
+
+    result
+
+  }
+
+  def sliceMatrix(matrix: CSCMatrix[Double], x: Int, y: Int) = matrix(Math.max(0, x - 1) to Math.min(matrix.rows - 1, x + 1), Math.max(0, y - 1) to Math.min(matrix.cols - 1, y + 1))
 
 
 }
