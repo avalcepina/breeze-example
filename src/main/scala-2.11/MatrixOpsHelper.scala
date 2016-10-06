80dcf009e20ff3be7a65a524c829351fe2d987dc
Alessandro Valcepina <valcepina@gmail.com>
2016-10-06 10:49:14 +0200

    Added tests on large matrices

--- src/main/scala-2.11/MatrixOpsHelper.scala
+++ src/main/scala-2.11/MatrixOpsHelper.scala
@@ -8,9 +8,6 @@ import breeze.linalg.{CSCMatrix, View, _}
   */
 object MatrixOpsHelper {
 
-
-  //implicit val bf = implicitly[CanMapKeyValuePairs[CSCMatrix[Double],(Int, Int),Double,Double,Double]]
-
   /**
     * Get a matrix from a file
     *
@@ -19,10 +16,10 @@ object MatrixOpsHelper {
     */
   def getMatrix(path: String) = csvread(new File(getClass.getResource(path).getPath), ',')
 
-  private def filterValue(x: Double, range: Range) = if (x <= range.inclusive.start || x >= range.inclusive.end) 0.0 else x
+  private def filterValue(x: Double, range: Range) = if (x <= range.start || x >= range.end) 0.0 else x
 
 
-  private def isInvalidValue(x: Double, range: Range) = (x <= range.start || x >= range.end)
+  private def isInvalidValue(x: Double, range: Range) = (x < range.start || x > range.end)
 
 
   /**
@@ -41,7 +38,7 @@ object MatrixOpsHelper {
     * @param range
     * @return
     */
-  def validateMatrix(matrix: CSCMatrix[Double], range: Range) = !matrix.flatten(View.Copy).exists(isInvalidValue(_, range))
+  def validateMatrix(matrix: CSCMatrix[Double], range: Range) = !matrix.flatten(View.Prefer).exists(isInvalidValue(_, range))
 
   /**
     * Calculates the average using surrounding elements of each element
@@ -54,42 +51,12 @@ object MatrixOpsHelper {
     val result = CSCMatrix.zeros[Double](matrix.rows, matrix.cols)
 
     matrix.pairs.foreach(v => {
-      result(v._1._1, v._1._2) = calculateSum(v._2, sliceMatrix(matrix, v._1._1, v._1._2).flatten(View.Copy))
+      result(v._1._1, v._1._2) = calculateSum(v._2, sliceMatrix(matrix, v._1._1, v._1._2).flatten(View.Prefer))
     })
 
     result
 
   }
-
-  // TODO maybe investigate more to try and implement the nicer solution
-  //  def betterMatrixAverage(matrix: CSCMatrix[Double]) = {
-
-  //
-  //    CSCMatrix.zeros[Double](matrix.rows, matrix.cols).active.pa
-  //
-  //    //
-  //
-  //    matrix.pairs.iterator.map(v => {
-  //
-  //      calculateSum(v._2, sliceMatrix(matrix, v._1._1, v._1._2).flatten(View.Copy))
-  //
-  //    }).toArray.foreach(v => println(v))
-  //
-  //    matrix.pairs.iterator.map(v => {
-  //      calculateSum(v._2, sliceMatrix(matrix, v._1._1, v._1._2).flatten(View.Copy))
-  //    }).toArray.grouped(matrix.cols).foreach(v => {
-  //      print("(")
-  //      v.foreach(c => print(c+","))
-  //      print(")")
-  //    }
-  //
-  //    CSCMatrix.create(matrix.cols, matrix.rows,
-  //      matrix.pairs.iterator.map(v => {
-  //
-  //        calculateSum(v._2, sliceMatrix(matrix, v._1._1, v._1._2).flatten(View.Copy))
-  //      }).toArray)
-  //
-  //  }
 
   private def sliceMatrix(matrix: CSCMatrix[Double], x: Int, y: Int) = matrix(Math.max(0, x - 1) to Math.min(matrix.rows - 1, x + 1), Math.max(0, y - 1) to Math.min(matrix.cols - 1, y + 1))
 
--- src/test/scala-2.11/MatrixOpsHelperSpec.scala
+++ src/test/scala-2.11/MatrixOpsHelperSpec.scala
@@ -1,10 +1,16 @@
-import breeze.linalg.CSCMatrix
-import org.scalatest.FunSuite;
+import breeze.linalg.{CSCMatrix, DenseMatrix, View}
+import org.scalatest.FunSuite
+import org.scalatest.concurrent.TimeLimitedTests;
+import org.scalatest.time.SpanSugar._
+
 
 /**
   * Created by avalcepina on 05/10/2016.
   */
-class MatrixOpsHelperSpec extends FunSuite {
+class MatrixOpsHelperSpec extends FunSuite with TimeLimitedTests {
+
+  //Time limit that should not be exceeded
+  val timeLimit = 10000 millis
 
   test("Should read matrix from file correctly") {
 
@@ -16,7 +22,7 @@ class MatrixOpsHelperSpec extends FunSuite {
 
   test("Should find matrix to be valid") {
 
-    val m = CSCMatrix((1.0, 2.0, 3.0), (1.0, 6.0, 7.0), (4.0, 8.0, 9.0))
+    val m = CSCMatrix((0.0, 2.0, 3.0), (1.0, 6.0, 7.0), (4.0, 8.0, 10.0))
 
     assert(MatrixOpsHelper.validateMatrix(m, 0 to 10))
 
@@ -52,19 +58,35 @@ class MatrixOpsHelperSpec extends FunSuite {
 
   }
 
-//  test("Should calculated better averages correctly") {
-//
-//    val m = CSCMatrix((1.0, 2.0, 3.0), (1.0, 2.0, 3.0), (1.0, 2.0, 3.0))
-//
-//    val expected = CSCMatrix((1.4, 2.0, 2.6), (1.4285714285714286, 2.0, 2.5714285714285716), (1.4, 2.0, 2.6))
-//
-//    println(m.toDense)
-//    println(MatrixOpsHelper.betterMatrixAverage(m).toDense)
-//
-//
-//    assert(MatrixOpsHelper.betterMatrixAverage(m).equals(expected))
-//
-//  }
+  test("Should calculated averages correctly on large matrix") {
 
+    val m = MatrixOpsHelper.getMatrix("/testSparseMatrix.csv")
+
+    val m2 = CSCMatrix.create[Double](m.rows, m.cols, m.toArray)
+
+    MatrixOpsHelper.matrixAverage(m2)
+
+  }
+
+
+  test("Should find large matrix to be valid") {
+
+    val m = MatrixOpsHelper.getMatrix("/testSparseMatrix.csv")
+
+    val m2 = CSCMatrix.create[Double](m.rows, m.cols, m.toArray)
+
+    assert(MatrixOpsHelper.validateMatrix(m2, -100 to 100))
+
+  }
+
+  test("Should filter correctly on large matrix") {
+
+    val m = MatrixOpsHelper.getMatrix("/testSparseMatrix.csv")
+
+    val m2 = CSCMatrix.create[Double](m.rows, m.cols, m.toArray)
+
+    MatrixOpsHelper.filterMatrix(m2, 0 to 20)
+
+  }
 
 }
